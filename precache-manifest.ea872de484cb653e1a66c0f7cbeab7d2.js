4bfbc65e0e770c562fcbe3862364bdfdcc65b03b
Abhay Pratap Singh <abhaypratap3536@gmail.com>
2020-05-02 18:06:47 +0100

    Updates

---
+++ precache-manifest.ea872de484cb653e1a66c0f7cbeab7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9492be571ff6d878d09",
+    "url": "/movie-catalog/static/css/main.7d5e6fcc.chunk.css"
+  },
+  {
+    "revision": "c9492be571ff6d878d09",
+    "url": "/movie-catalog/static/js/main.7736a0c9.chunk.js"
+  },
+  {
+    "revision": "ea18e19d3ddafb3ff9bd",
+    "url": "/movie-catalog/static/js/runtime~main.599fc602.js"
+  },
+  {
+    "revision": "d3c322974390c128c33d",
+    "url": "/movie-catalog/static/js/2.83f3be23.chunk.js"
+  },
+  {
+    "revision": "d1b4253f16e6f9d9cf9cc5a0af03da24",
+    "url": "/movie-catalog/static/media/placeholder.d1b4253f.png"
+  },
+  {
+    "revision": "bfd6b8a2fbaef405a1d6f3e114214c7b",
+    "url": "/movie-catalog/index.html"
+  }
+];
